ab7a1bb7344746f353d98a4f6f026b815b820f93
Phil Wilson <github@philwilson.org>
2013-09-21 22:52:30 +0100

    tidy up imports

--- src/org/philwilson/huffduffer/ItemListFragment.java
+++ src/org/philwilson/huffduffer/ItemListFragment.java
@@ -1,7 +1,5 @@
 package org.philwilson.huffduffer;
 
-import org.philwilson.huffduffer.dummy.DummyContent;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
